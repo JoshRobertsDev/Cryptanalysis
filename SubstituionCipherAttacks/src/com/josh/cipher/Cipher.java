e138a18f4ca2eb5aeeaa98b26b8178eb437ee693
Josh Roberts <josh.roberts@live.co.uk>
2014-03-08 17:47:08 +0000

    Changed Cipher to static class

--- SubstituionCipherAttacks/src/com/josh/cipher/Cipher.java
+++ SubstituionCipherAttacks/src/com/josh/cipher/Cipher.java
@@ -1,15 +1,13 @@
 package com.josh.cipher;
 
+/*
+ * @author Josh Roberts 
+ */
 public class Cipher {
 	
-	public char[] substitute(String inputText, char[] alphabet, char[] cipherAlphabet) {
+	public static char[] substitute(String inputText, char[] alphabet, char[] cipherAlphabet) {
 		
 		char[] cipherTextArray = inputText.toCharArray();
-		
-		//System.out.println(cipherTextArray.length);
-		//System.out.println(cipherTextArray);
-		//System.out.println(alphabet.length + " | " + cipherAlphabet.length);
-		//System.out.println(alphabet[1] + " | " + cipherAlphabet[1]);
 		
 		for(int i = 0; i < cipherTextArray.length; i++) {
 			for(int j = 0; j < 26; j++) {
