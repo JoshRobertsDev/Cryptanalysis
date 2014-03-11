d02d69743c7175450114b69e35807ac19db2e2f1
Josh Roberts <josh.roberts@live.co.uk>
2014-03-11 12:21:18 +0000

    Optimised main method to run each algorithm for a set time and output to results to file

--- SubstituionCipherAttacks/src/com/josh/main/Main.java
+++ SubstituionCipherAttacks/src/com/josh/main/Main.java
@@ -4,9 +4,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.josh.cipher.Alphabet;
 import com.josh.cipher.Cipher;
+import com.josh.cipher.Fitness;
 import com.josh.genetic.Genetic;
 import com.josh.hc.HillClimb;
 import com.josh.pso.Particle;
@@ -22,7 +25,7 @@ public class Main {
 		key.Scramble();
 		int noScoreImprovementLimit = 10000;
 		
-		String plaintext = "TWO HOUSEHOLDS BOTH ALIKE IN DIGNITY IN FAIR VERONA WHERE WE LAY OUR SCENE FROM ANCIENT GRUDGE BREAK TO NEW MUTINY WHERE CIVIL BLOOD MAKES CIVIL HANDS UNCLEAN FROM FORTH THE FATAL LOINS OF THESE TWO FOES A PAIR OF STAR CROSSD LOVERS TAKE THEIR LIFE WHOSE MISADVENTURED PITEOUS OVERTHROWS DO WITH THEIR DEATH BURY THEIR PARENTS STRIFE THE FEARFUL PASSAGE OF THEIR DEATH MARKD LOVE AND THE CONTINUANCE OF THEIR PARENTS RAGE WHICH BUT THEIR CHILDRENS END NOUGHT COULD REMOVE IS NOW THE TWO HOURS TRAFFIC OF OUR STAGE THE WHICH IF YOU WITH PATIENT EARS ATTEND WHAT HERE SHALL MISS OUR TOIL SHALL STRIVE TO MEND";
+		String plaintext = "TWO HOUSEHOLDS BOTH ALIKE IN DIGNITY IN FAIR VERONA WHERE WE LAY OUR SCENE FROM ANCIENT GRUDGE BREAK TO NEW MUTINY WHERE CIVIL BLOOD MAKES CIVIL HANDS UNCLEAN FROM FORTH THE FATAL LOINS OF THESE TWO FOES A PAIR OF STAR CROSSED LOVERS TAKE THEIR LIFE WHOSE MISADVENTURE PITEOUS OVERTHROWS DO WITH THEIR DEATH BURY THEIR PARENTS STRIFE THE FEARFUL PASSAGE OF THEIR DEATH MARKED LOVE AND THE CONTINUANCE OF THEIR PARENTS RAGE WHICH BUT THEIR CHILDREN END NOUGHT COULD REMOVE IS NOW THE TWO HOURS TRAFFIC OF OUR STAGE THE WHICH IF YOU WITH PATIENT EARS ATTEND WHAT HERE";
 		//String plaintext = "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG CAT SAT ON MAT DOG CHASED CAT";
 		String cipherText = new String(Cipher.substitute(plaintext, alphabet.getAlphabet(), key.getAlphabet()));
 	/*
@@ -34,11 +37,30 @@ public class Main {
 		System.out.println("Results: " + timeTaken + "s using " + hc.iterations + " iterations at " + hc.iterations/timeTaken + " i/s");
 	*/
 		
-		Genetic genetic = new Genetic();
-		String output = "";
-				
-				genetic.searchOptimalKey(20, 120, cipherText, 10);
+		Fitness fitness = new Fitness();
+		fitness.setCipherText(cipherText);
+		System.out.println(fitness.score(key.getAlphabet()));
 		
+		List<Double[]> resultsList = new ArrayList<>();		
+		
+		for(int i = 0; i < 3; i++) {
+			Genetic genetic = new Genetic();
+			resultsList.add(genetic.searchOptimalKey(20, 10, cipherText, 10));
+			System.out.println("Iteration: " + (i+1));
+		}
+		
+		String output = "";
+		for(int i = 0; i < resultsList.get(0).length; i++) {
+			output += i + ",";
+			for(int j = 0; j < resultsList.size(); j++) {
+				output += resultsList.get(j)[i];
+				if(j != (resultsList.size()-1)) {
+					output += ",";
+				}
+			}
+			output += "\n";
+		}
+				
 		try {
 			File results = new File("results.txt");
 			if(!results.exists()) 
@@ -50,16 +72,8 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		
-		
-
-		
-		
-		
 		
 		//System.out.println(new String(key.getAlphabet()));
-	
 		//start = System.currentTimeMillis();
 		//ParticleSwarm ps = new ParticleSwarm();
 		//ps.searchOptimalKey(500, 20, cipherText, 0.2, 0.1);
