8d31aa835c668d6e72ff0e32e5e6e0b056f51b9b
Josh Roberts <josh.roberts@live.co.uk>
2014-03-16 22:47:45 +0000

    Frequency Analysis

--- SubstituionCipherAttacks/src/com/josh/cipher/FrequencyAnalyse.java
+++ SubstituionCipherAttacks/src/com/josh/cipher/FrequencyAnalyse.java
@@ -1,10 +1,22 @@
 package com.josh.cipher;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class FrequencyAnalyse {
-	public static HashMap<Character, Integer>  monograms(String cipherText) {
-		HashMap<Character,Integer> frequencyMap = new HashMap<Character,Integer>();          
+	
+	private final static Character[] frequency = {'E','T','A','O','I','R','H','N','S','L','D','C','U','M','F','G','P','W','Y','B','V','K','J','Q','X','Z'};
+	
+	public static List<Character>  monograms(String cipherText) {
+		cipherText = cipherText.replaceAll(" ", "");
+		Map<Character,Integer> frequencyMap = new HashMap<Character,Integer>();          
 		for(int i = 0; i < cipherText.length(); i++){
 		   char c = cipherText.charAt(i);
 		   Integer val = frequencyMap.get(new Character(c));
@@ -14,6 +26,65 @@ public class FrequencyAnalyse {
 			   frequencyMap.put(c,1);
 		   }
 		}
-		return frequencyMap;
+		return sortByValue(frequencyMap);
+	}
+	
+	private static List<Character> sortByValue(Map<Character, Integer> frequencyMap) {
+		List<Entry<Character, Integer>> list = new LinkedList<Entry<Character, Integer>>(frequencyMap.entrySet());
+		List<Character> key = new ArrayList<>();
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<Character, Integer>>()
+        {
+            public int compare(Entry<Character, Integer> o1,
+                    Entry<Character, Integer> o2)
+            {
+            	return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        // Maintaining insertion order with the help of LinkedList
+        //Map<Character, Integer> sortedMap = new LinkedHashMap<Character, Integer>();
+        for (Entry<Character, Integer> entry : list)
+        {
+        	key.add(entry.getKey());
+            //sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return key;
+	}
+	
+	public static char[] getKey(String cipherText) {
+		List<Character> expectedFrequency = Arrays.asList(frequency);
+		List<Character> frequencyList = monograms(cipherText);
+		List<Character> missingLetters = new ArrayList<>();
+		List<Character> charList = new ArrayList<>();
+		char[] key = new char[26];
+		
+		System.out.println(expectedFrequency + "\n" + frequencyList);
+		
+		int count = 0;
+		for(Character i = 'A'; i <= 'Z'; i++) {
+			int index = expectedFrequency.indexOf(i);
+			if(index < frequencyList.size()) {
+				key[count] = frequencyList.get(index);
+				count++;
+			}
+		}
+		
+		for(int i = 0; i < key.length; i++) {
+			charList.add(key[i]);
+		}
+		
+    	for(char letter = 'A'; letter <= 'Z'; letter++) {
+    		if(!charList.contains(letter))
+    			missingLetters.add(letter);
+    	}
+    	
+    	for(int i = 0; i < charList.size(); i++) {
+    		if(charList.get(i) == '\0') {
+    			key[i] = missingLetters.get(0);
+    			missingLetters.remove(0);
+    		} 
+    	}
+		return key;
 	}
 }
